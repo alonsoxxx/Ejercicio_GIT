15946268394777313e120fff0a1344b34acedd5c
alonsoxxx <89150675+alonsoxxx@users.noreply.github.com>
2026-01-22 20:45:27 -0500

    chore: restaurar Program.cs

--- Ejercicio_GIT/Program.cs
+++ Ejercicio_GIT/Program.cs
@@ -11,38 +11,32 @@ namespace Ejercicio_GIT
         static void Main(string[] args)
         {
             double resultado, num1 = 10, num2 = 20;
-            int opcion;
 
-            do
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicación");
+
+            Console.Write("Elige una opción: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
             {
-                Console.WriteLine("1. Suma");
-                Console.WriteLine("2. Resta");
-                Console.WriteLine("3. Multiplicación");
-                Console.WriteLine("4. Salir");
-
-                Console.Write("Elige una opción: ");
-                opcion = Convert.ToInt32(Console.ReadLine());
-
-                switch (opcion)
-                {
-                    case 1:
-                        resultado = Suma(num1, num2);
-                        Console.WriteLine($"Resultado de la suma: {resultado}");
-                        break;
-                    case 2:
-                        resultado = Resta(num1, num2);
-                        Console.WriteLine($"Resultado de la resta: {resultado}");
-                        break;
-                    case 3:
-                        resultado = Multiplicacion(num1, num2);
-                        Console.WriteLine($"Resultado de la multiplicación: {resultado}");
-                        break;
-                    default:
-                        Console.WriteLine("Opción no válida.");
-                        break;
-                }
-            } while (opcion != 4);
-
+                case 1:
+                    resultado = Suma(num1, num2);
+                    Console.WriteLine($"Resultado de la suma: {resultado}");
+                    break;
+                case 2:
+                    resultado = Resta(num1, num2);
+                    Console.WriteLine($"Resultado de la resta: {resultado}");
+                    break;
+                case 3:
+                    resultado = Suma(num1, num2);
+                    Console.WriteLine($"Resultado de la multiplicación: {resultado}");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
 
         }
         static double Suma (double num1, double num2)
